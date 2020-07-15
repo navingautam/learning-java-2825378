f1cf8dad2168f584ec2549453ac36a39624e5648
gautamn <navin.gautam@tabcorp.com.au>
2020-07-15 20:51:14 +1000

    functions coding challenge

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static double calculateGrossSalary(double hoursPerWeek, double ratePerHour, double vacationDays){
+        if(hoursPerWeek < 0 || ratePerHour < 0 || vacationDays < 0){
+            return -1;
+        }
+
+        double weeklyPayCheck = hoursPerWeek * ratePerHour;
+        double unpaidTime = vacationDays * 8;
+
+        return weeklyPayCheck * 52 - unpaidTime * ratePerHour;
+
+    }
+
     public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
+        System.out.println("Enter the number of hours per week ");
+        Scanner scanner = new Scanner(System.in);
+        double hoursPerWeek = scanner.nextDouble();
+
+        System.out.println("Enter the rate per hour ");
+        double ratePerHour = scanner.nextDouble();
+
+        System.out.println("Enter vacation days ");
+        double vacationDays = scanner.nextDouble();
+
+        double grossYearlySalary = calculateGrossSalary(hoursPerWeek, ratePerHour, vacationDays);
+        System.out.println(grossYearlySalary);
+
     }
 
 }
